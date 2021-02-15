67933a0ed95fd8da43abc0012c88322736d6c5b6
Kailash Chandra <KChandra@BHAVNACORP.NET>
2021-02-15 10:42:51 +0530

    add postname in post class

--- CoreServices/Models/Post.cs
+++ CoreServices/Models/Post.cs
@@ -8,6 +8,8 @@ namespace CoreServices.Models
     public partial class Post
     {
         public int PostId { get; set; }
+
+        public int PostName { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int? CategoryId { get; set; }
